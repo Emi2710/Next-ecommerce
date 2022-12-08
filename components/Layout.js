8d3d462be8b0293dd9a2ec2578cd17d7762abf4d
Emi2710 <deadourfreedom@gmail.com>
2022-12-08 01:40:05 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -282,15 +282,16 @@ export default function Layout({ title, description, children }) {
                     </Script>
 
                     <Script id="gt-change-content">
-                      {`var targetElement = document.getElementById("google_translate_element");
-                        var yourContent = "Your text or <br /> HTML element"
-                        targetElement.addEventListener("DOMNodeInserted", () => {
-                          $(".goog-te-menu-value span:first").each(function (i, obj) {
-                            if (obj.childNodes[0] && obj.childNodes[0].textContent == "Langue") {
-                              $(obj).html(yourContent);
-                            }
-                          });
-                        });`}
+                      {`$(document).ready(function(){
+    $('#google_translate_element').bind('DOMNodeInserted', function(event) {
+      $('.goog-te-menu-value span:first').html('LANGUAGE');
+      $('.goog-te-menu-frame.skiptranslate').load(function(){
+        setTimeout(function(){
+          $('.goog-te-menu-frame.skiptranslate').contents().find('.goog-te-menu2-item-selected .text').html('LANGUAGE');    
+        }, 100);
+      });
+    });
+  });`}
                     </Script>
 
                   
